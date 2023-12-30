ab52c28ab8a0f98e850d2ee0ad9fcba78ab74ae2
Joseph Murche <jmurche@yahoo.com>
2023-12-30 00:22:47 -0800

    Updated routes with cert checker. Not complete yet.

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -3,6 +3,8 @@ package routes
 import (
 	"log-collection-api/pkg/middleware"
 	"net/http"
+	"os"
+	"fmt"
 
 	jwtmiddleware "github.com/auth0/go-jwt-middleware/v2"
 	"github.com/auth0/go-jwt-middleware/v2/validator"
@@ -47,5 +49,38 @@ func Router(audience, domain string) http.Handler {
     ))
 
     return router
+}
+
+func userHasCertificate(userID string) bool {
+	//template
+	return false 
+}
+
+//basic implementation. will continue to work on it
+func downloadHandler(w http.ResponseWriter, r *http.Request) {
+	userID := r.URL.Query().Get("userID")
+
+	if userID == "" {
+		http.Error(w, "User ID is required", http.StatusBadRequest)
+		return
+	}
+
+	if !userHasCertificate(userID) {
+		http.Error(w, "No certificate found for the user", http.StatusNotFound)
+		return
+	}
+
+	certificatePath := fmt.Sprintf("/path/to/certificates/%s-certificate.pdf", userID)
+
+	file, err := os.Open(certificatePath)
+	if err != nil {
+		http.Error(w, "Error opening the certificate file", http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s-certificate.pdf", userID))
+	w.Header().Set("Content-Type", "application/pdf")
+	http.ServeContent(w, r, certificatePath, file.Stat().ModTime(), file)
 }
 
